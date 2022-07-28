0bdbde7ba304987a18ea3ce28ff04572c151307c
Reethu Thota <110167324+reethuthota@users.noreply.github.com>
2022-07-28 11:05:37 +0000

    area of triangle

--- p0original.c
+++ p0original.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-int main(void)
+int main()
 {
-  printf("my name is reethu\n");
+  float length = 10.0;
+  float breadth = 20.0;
+  float area = length * breadth;
+  printf("the area of the triangle with length = %f and breadth is %f is %f\n",length,breadth,area);
   return 0;
 }
